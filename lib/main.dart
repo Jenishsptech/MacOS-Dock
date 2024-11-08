b58e6e85dcf94071bbc636d3540620ab366584eb
spsofttech <85215401+Max1881@users.noreply.github.com>
2024-11-08 10:04:04 +0530

    build

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() => runApp(DockerApp());
 class DockerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: DockerAnimation(),
